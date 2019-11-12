0cf5f67dca5a7d73a00f8f48b8c4541f3e673e4b
Favian <damin8@naver.com>
2019-11-12 19:51:56 +0900

    2019-11-12

--- Server(java)/src/Music/MusicModel.java
+++ Server(java)/src/Music/MusicModel.java
@@ -32,84 +32,26 @@ public class MusicModel {
 			// this.musicSource = new
 			// DataSource("C:\\Users\\damin\\eclipse-workspace\\WekaTest\\bin\\music.arff");
 			this.musicSource = new
-			DataSource("C:\\Users\\damin\\eclipse-workspace\\MusicPrediction\\weka_source\\MusicPredict.arff");
+			DataSource("MusicPredict.arff");
 			this.musicInstance = this.musicSource.getDataSet();
-			//int num = musicInstance.numInstances() / 10;
-			//Instances[] tenInstance = new Instances[num+1];
-			//System.out.println("Num : " + num);
 			this.musicInstance.setClassIndex(musicInstance.numAttributes() - 1);
-			// 10개씩 묶는 작업
-			/*int count = 0;
-			for (int i = num; i < musicInstance.numInstances(); i+=num) {
-				tenInstance[count] = 
-						new weka.core.Instances(musicInstance,0,i-1);
-				count++;
-			}*/
+			//tenInstance[count] = new weka.core.Instances(musicInstance,0,i-1);
 			this.musicOneR = new OneR();
 			this.musicNaiveBayes = new NaiveBayes();
 			this.musicTree = new J48();
-			/*double[] accuracy = new double[count];
-			for (int i = 0; i < count; i++) {
-				musicOneR.buildClassifier(tenInstance[i]);
-				musicOneREval = new Evaluation(tenInstance[i]);
-				musicOneREval.crossValidateModel(musicOneR, tenInstance[i], 10, new Random(1));
-				accuracy[i] = musicOneREval.pctCorrect();
-			}
-			String str = "[";
-			for (int i = 0; i < count; i++) {
-				// System.out.println("OneR " + i+1 +" 번째 정확도 : " + accuracy[i]);
-				if (i == (count - 1))
-					str += (accuracy[i] + " ]");
-				else
-					str += (accuracy[i] + ", ");
-			}
-			System.out.println("OneR = " + str);*/
 			this.musicOneR.buildClassifier(musicInstance);
 			this.musicOneREval = new Evaluation(musicInstance);
 			this.musicOneREval.crossValidateModel(musicOneR, musicInstance, 10, new Random(1));
-			// System.out.println(musicOneREval.toSummaryString());
 			// double accuracy = musicOneREval.pctCorrect(); 정확도 추출
-			// System.out.println(accuracy);
-			/*for (int i = 0; i < count; i++) {
-				musicNaiveBayes.buildClassifier(tenInstance[i]);
-				musicNaiveEval = new Evaluation(tenInstance[i]);
-				musicNaiveEval.crossValidateModel(musicNaiveBayes, tenInstance[i], 10, new Random(1));
-				accuracy[i] = musicNaiveEval.pctCorrect();
-			}
-			str = "[";
-			for (int i = 0; i < count; i++) {
-				// System.out.println("NaiveBayesian " + i+1 +" 번째 정확도 : " + accuracy[i]);
-				if (i == (count - 1))
-					str += (accuracy[i] + " ]");
-				else
-					str += (accuracy[i] + ", ");
-			}
-			System.out.println("NaiveBayesian = " + str);*/
 			this.musicNaiveBayes.buildClassifier(musicInstance);
 			// Evaluation on Training Set
-			// Movie NaiveBayes Cross-Validation
+			// Music NaiveBayes Cross-Validation
 			this.musicNaiveEval = new Evaluation(musicInstance);
 			this.musicNaiveEval.crossValidateModel(musicNaiveBayes, musicInstance, 10, new Random(1));
-
-			// Movie J48 Training
-			/*for (int i = 0; i < count; i++) {
-				musicTree.buildClassifier(tenInstance[i]);
-				musicTreeEval = new Evaluation(tenInstance[i]);
-				musicTreeEval.crossValidateModel(musicTree, tenInstance[i], 10, new Random(1));
-				accuracy[i] = musicTreeEval.pctCorrect();
-			}
-			str = "[";
-			for (int i = 0; i < count; i++) {
-				//System.out.println("Tree " + i + 1 + " 번째 정확도 : " + accuracy[i]);
-				if (i == (count - 1))
-					str += (accuracy[i] + " ]");
-				else
-					str += (accuracy[i] + ", ");
-			}
-			System.out.println("Decision tree : " + str);*/
+			// Music J48 Training
 			this.musicTree.buildClassifier(musicInstance);
 			// Evaluation on Training Set
-			// Movie J48 Cross-Validation
+			// Music J48 Cross-Validation
 			this.musicTreeEval = new Evaluation(musicInstance);
 			this.musicTreeEval.crossValidateModel(musicTree, musicInstance, 10, new Random(1));
 
