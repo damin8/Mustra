ef0c5016d943a7298146428d7e656b9953245697
Favian <damin8@naver.com>
2019-12-02 19:23:01 +0900

    update
    사진들 업데이트
    
    +
    
    정보 업데이트

--- Client(C#)/Mustra/ViewModel/OneRuleCMUserControViewModel.cs
+++ Client(C#)/Mustra/ViewModel/OneRuleCMUserControViewModel.cs
@@ -33,8 +33,8 @@ namespace Mustra.ViewModel
         }
         public OneRuleCMUserControViewModel()
         {
-            this.OneRuleCMString = "fan:\n\t < 10070.5\t->D\n\t < 19978.5\t->C\n\t < 24222.5\t->D\n\t < 67431.0\t->C\n\t < 177787.0\t->B\n\t < 652940.0\t->A\n\t >= 652940.0\t->B\n(444 / 515 instances correct)\n";
-            this.OneRuleCMString2 = "\nCorrectly Classified Instances         429               83.301  %\nIncorrectly Classified Instances        86               16.699  %\nKappa statistic                          0.5318\nMean absolute error                      0.0835\nRoot mean squared error                  0.289 \nRelative absolute error                 43.9419 %\nRoot relative squared error             94.1366 %\nTotal Number of Instances              515     \n";
+            this.OneRuleCMString = "fan:\n\t< 10070.5\t-> D\n\t< 67431.0\t-> C\n\t< 177787.0\t-> B\n\t< 652940.0\t-> A\n\t>= 652940.0\t-> B\n(437/509 instances correct)\n";
+            this.OneRuleCMString2 = "\nCorrectly Classified Instances         428               84.0864 %\nIncorrectly Classified Instances        81               15.9136 %\nKappa statistic                          0.5563\nMean absolute error                      0.0796\nRoot mean squared error                  0.2821\nRelative absolute error                 42.0074 %\nRoot relative squared error             92.0466 %\nTotal Number of Instances              509     \n";
             this.OneRuleCMString += this.OneRuleCMString2;   
         }
         protected void OnPropertyChanged(string name)
