d9797d1dd229ee0f110515f0aad2b79173cf85da
Favian <damin8@naver.com>
2020-05-28 16:24:12 +0900

    Update Async
    - 비동기 처리를 위한 SpringAsyncConfig
    
    - Return Type = CompletableFuture로 변경 (비동기)

---
+++ MusicPrediction(Spring boot)/src/main/java/mustra/musicprediction/Configuration/SpringAsyncConfig.java
@@ -0,0 +1,46 @@
+package mustra.musicprediction.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig {
+    @Bean(name = "findAllFeedThread")
+    public Executor findAllFeedThread() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5); // 기본 스레드 개수
+        taskExecutor.setMaxPoolSize(100); // 큐카파 넘어가면 스레드 축 생성
+        taskExecutor.setQueueCapacity(30); // 스레드 대기 큐
+        taskExecutor.setThreadNamePrefix("findAllFeedThread");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+
+    @Bean(name = "createFeedThread")
+    public Executor createFeedThread() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5); // 기본 스레드 개수
+        taskExecutor.setMaxPoolSize(100); // 큐카파 넘어가면 스레드 축 생성
+        taskExecutor.setQueueCapacity(30); // 스레드 대기 큐
+        taskExecutor.setThreadNamePrefix("createFeedThread");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Bean(name = "findRankThread")
+    public Executor findRankThread() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5); // 기본 스레드 개수
+        taskExecutor.setMaxPoolSize(100); // 큐카파 넘어가면 스레드 축 생성
+        taskExecutor.setQueueCapacity(30); // 스레드 대기 큐
+        taskExecutor.setThreadNamePrefix("findRankThread");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
--- MusicPrediction(Spring boot)/src/main/java/mustra/musicprediction/Controller/MustraController.java
+++ MusicPrediction(Spring boot)/src/main/java/mustra/musicprediction/Controller/MustraController.java
@@ -8,15 +8,19 @@ import mustra.musicprediction.Repository.FeedRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.annotation.WebServlet;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/mustra/*")
+@WebServlet(asyncSupported = true)
 public class MustraController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -38,29 +42,32 @@ public class MustraController {
         return str;
     }
 
+    @Async(value="findRankThread")
     @RequestMapping(value = "/find/rank")
     public String findRank(@RequestBody Music music){
         return "HI";
     }
 
+    @Async(value="createFeedThread")
     @RequestMapping(value = "/feed/create", method = RequestMethod.POST)
-    public String createFeed(@RequestBody Feed feed){
+    public CompletableFuture<String> createFeed(@RequestBody Feed feed){
         Feed temp = feedRepository.insert(feed);
         Gson gson = new Gson();
         String json = gson.toJson(temp);
         logger.info("=====createFeed=====");
         logger.info("Request = " + feed.toString());
         logger.info("Response = " + json);
-        return json;
+        return CompletableFuture.completedFuture(json);
     }
 
+    @Async(value="findAllFeedThread")
     @RequestMapping(value = "/feed/findAll", method = RequestMethod.GET)
-    public List<Feed> fildAllFeed(){
+    public CompletableFuture<String> fildAllFeed(){
         List<Feed> feedList = feedRepository.findAll();
         Gson gson = new Gson();
         String json = gson.toJson(feedList);
         logger.info("=====findAllFeed=====");
         logger.info("Response = " + json);
-        return feedList;
+        return CompletableFuture.completedFuture(json);
     }
 }
